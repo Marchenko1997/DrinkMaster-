12d939ac9dec64180793bd083f2a1770856bc720
Halyna Marchenko <marchenkohalyna888@gmail.com>
2024-11-22 20:32:06 +0100

    set up DatePicker

---
+++ src/components/SignUp/SignUpForm.jsx
@@ -0,0 +1,8 @@
+// import { Formik, ErrorMessage } from "formik";
+// import { ToastContainer, toast, Slide } from "react-toastify"; 
+// import "react-toastify/dist/ReactToastify.css";
+// import { useState } from "react";
+// import { useDispatch } from "react-redux";
+// import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+// import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+// import * as Yup from "yup";
