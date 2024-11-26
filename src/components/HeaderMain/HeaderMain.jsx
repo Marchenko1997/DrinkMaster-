cc2410a17d0903f05aeb98bc60f364c9a27775b6
Halyna Marchenko <marchenkohalyna888@gmail.com>
2024-11-26 19:27:09 +0100

    set up users menu

--- src/components/HeaderMain/HeaderMain.jsx
+++ src/components/HeaderMain/HeaderMain.jsx
@@ -2,15 +2,19 @@ import { MainHeader, MobileBox, NavigationHeader } from "./Header.styled";
 import LogoHeader from "./LogoHeader/LogoHeader";
 import NavMenu from "./NavMenu/NavMenu";
 import ToggleDesktop from "./ToggleDesktop/ToggleDesktop";
+import UserMenu from "./UserMenu/UserMenu";
+import { useState } from "react";
 
 const HeaderMain = () => {
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
   return (
     <MainHeader>
       <NavigationHeader>
         <LogoHeader />
         <NavMenu />
-              <MobileBox>
-            <ToggleDesktop/>
+        <MobileBox>
+          <ToggleDesktop isOpenMenu={isOpenMenu} />
+          {!isOpenMenu && <UserMenu />}
         </MobileBox>
       </NavigationHeader>
     </MainHeader>
---
+++ src/components/HeaderMain/UserMenu/UserMenu.jsx
@@ -0,0 +1,13 @@
+import { AvatarImage, UserMenuContainer, BtnUserMenu } from "./UserMenu.styled"
+
+const UserMenu = () => {
+  return (
+      <UserMenuContainer>
+          <BtnUserMenu>
+              <AvatarImage/>
+          </BtnUserMenu>
+   </UserMenuContainer>
+  )
+}
+
+export default UserMenu
