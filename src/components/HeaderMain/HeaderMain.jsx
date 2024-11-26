4b0f9a33c9002e86692471e8497b5fa2361be5bd
Halyna Marchenko <marchenkohalyna888@gmail.com>
2024-11-26 17:19:17 +0100

    adding icons.svg и LogoHeader

--- src/components/HeaderMain/HeaderMain.jsx
+++ src/components/HeaderMain/HeaderMain.jsx
@@ -1,9 +1,11 @@
 import { MainHeader, MobileBox, NavigationHeader } from "./Header.styled";
+import LogoHeader from "./LogoHeader/LogoHeader";
 
 const HeaderMain = () => {
     return (
         <MainHeader>
             <NavigationHeader>
+                <LogoHeader/>
                 <MobileBox></MobileBox>
         </NavigationHeader>
       </MainHeader>
---
+++ src/components/HeaderMain/LogoHeader/LogoHeader.jsx
@@ -0,0 +1,8 @@
+import sprite from "../../../assets/icons/icons.svg";
+import { SvgIcon, LogoLink, IconText } from "./LogoHeader.styled";
+
+const LogoHeader = () => {
+  return <div>LogoHeader</div>;
+};
+
+export default LogoHeader;
