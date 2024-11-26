d7a7d053a25a6d1d157b81093324061e4796ad7b
Halyna Marchenko <marchenkohalyna888@gmail.com>
2024-11-26 17:57:47 +0100

    creating Loader

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,9 +1,15 @@
 import HeaderMain from "../HeaderMain/HeaderMain";
+import { Suspense } from "react";
+import { Outlet } from "react-router-dom";
+import { Loader } from "../Loader/Loader";
 
 const SharedLayout = () => {
   return (
     <>
       <HeaderMain />
+      <Suspense fallback={<Loader />}>
+        <Outlet />
+      </Suspense>
     </>
   );
 };
