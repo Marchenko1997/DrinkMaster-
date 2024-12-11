9ed6a6fe6b5fd903bcc3c3bc1e013592663e7775
Halyna Marchenko <marchenkohalyna888@gmail.com>
2024-12-11 18:39:45 +0200

    setup searchIngredient

---
+++ src/components/DrinkSearch/Select/SearchSelectIngredients.jsx
@@ -0,0 +1,46 @@
+import { SearchSelectStyled } from "./SearchSelect.styled.jsx";
+
+
+const SearchSelectIngredients = ({ setIngridient }) => {
+    const searchByIngridient = (event) => { 
+        event.preventDefault();
+        const ingridientParam = event.target.value;
+        if (ingridientParam === "hide") setIngridient("");
+        else setIngridient(ingridientParam);
+    }
+  return (
+    <SearchSelectStyled>
+      <select
+        id="ingredients"
+        name="ingredients"
+        defaultValue="hide"
+        className="select select-styled"
+        onChange={searchByIngridient}
+      >
+        <option value="hide">Ingredients</option>
+        <option value="Light rum">Light rum</option>
+        <option value="Applejack">Applejack</option>
+        <option value="gin">Gin</option>
+        <option value="Dark rum">Dark rum</option>
+        <option value="Sweet Vermouth">Sweet Vermouth</option>
+        <option value="Strawberry">Strawberry Schnapps</option>
+        <option value="Scotch">Scotch</option>
+        <option value="Apricot brandy">Apricot brandy</option>
+        <option value="Triple sec">Triple sec</option>
+        <option value="Southern Comfort">Southern Comfort</option>
+        <option value="Orange bitters">Orange bitters</option>
+        <option value="Brandy">Brandy</option>
+        <option value="Lemon vodka">Lemon vodka</option>
+        <option value="Blended whiskey">Blended whiskey</option>
+        <option value="Dry Vermouth">Dry Vermouth</option>
+        <option value="Amaretto">Amaretto</option>
+        <option value="Tea">Tea</option>
+        <option value="Champagne">Champagne</option>
+        <option value="Coffee liqueur">Coffee liqueur</option>
+        <option value="Bourbon">Bourbon</option>
+      </select>
+    </SearchSelectStyled>
+  );
+}
+
+export default SearchSelectIngredients
--- src/pages/DrinksPage/DrinksPage.jsx
+++ src/pages/DrinksPage/DrinksPage.jsx
@@ -1,21 +1,24 @@
-import Title from '../../components/Title/Title'
-import { useState } from 'react'
-import SearchBar from '../../components/DrinkSearch/SearchBar/SearchBar'
-import { SearchingContainer } from './DrinksPage.styled'
-import SearchSelectCategory from '../../components/DrinkSearch/Select/SearchSelectCategory'
+import Title from "../../components/Title/Title";
+import { useState } from "react";
+import SearchBar from "../../components/DrinkSearch/SearchBar/SearchBar";
+import { SearchingContainer } from "./DrinksPage.styled";
+import SearchSelectCategory from "../../components/DrinkSearch/Select/SearchSelectCategory";
+import SearchSelectIngredients from "../../components/DrinkSearch/Select/SearchSelectIngredients";
 
 const DrinksPage = () => {
-    const [drink, setDrink] = useState("");
-    const [category, setCategory] = useState("");
+  const [drink, setDrink] = useState("");
+  const [category, setCategory] = useState("");
+  const [ingredient, setIngredient] = useState("");
   return (
-      <main className='container'>
-          <Title text={"Drinks"} />
-          <SearchingContainer>
-              <SearchBar setDrink={setDrink} />
-              <SearchSelectCategory setCategory={setCategory}/>
-          </SearchingContainer>
+    <main className="container">
+      <Title text={"Drinks"} />
+      <SearchingContainer>
+        <SearchBar setDrink={setDrink} />
+              <SearchSelectCategory setCategory={setCategory} />
+              <SearchSelectIngredients setIngridient={setIngredient} />
+      </SearchingContainer>
     </main>
-  )
-}
+  );
+};
 
-export default DrinksPage
+export default DrinksPage;
