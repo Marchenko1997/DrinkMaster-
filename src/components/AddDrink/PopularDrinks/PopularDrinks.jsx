a2cbbe23e3ff4e12903b33d05eafa8c502b802b0
DrunkCherry1997 <poisonivy1997@ukr.net>
2024-12-18 13:39:25 +0200

    finidhing aside container with popular drinks

--- src/components/AddDrink/PopularDrinks/PopularDrinks.jsx
+++ src/components/AddDrink/PopularDrinks/PopularDrinks.jsx
@@ -1,11 +1,53 @@
-import FollowUs from "../FollowUs/FollowUs"
+import FollowUs from "../FollowUs/FollowUs";
+import {
+  DrinkDescription,
+  NameDrink,
+  PopDrinkImage,
+  LinkDrink,
+  Content,
+  TitlePopularDrinks,
+  AsideContainer,
+  TitleFollowUs,
+} from "./PopularDrinks.styled";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchPopularDrinks } from "../../../redux/drinks/drinks.operations";
+import { selectPopularDrinks } from "/src/redux/selectors/drinks.selectors.js";
 
 const PopularDrinks = () => {
-  return (
-      <div>
-        <FollowUs />
-    </div>
-  )
-}
+  const popDrinks = useSelector(selectPopularDrinks);
+  const dispatch = useDispatch();
 
-export default PopularDrinks
+  useEffect(() => {
+    dispatch(fetchPopularDrinks());
+  }, [dispatch]);
+
+  return (
+    <AsideContainer>
+      <TitleFollowUs>Follow Us</TitleFollowUs>
+      <FollowUs />
+      <TitlePopularDrinks>Popular Drinks</TitlePopularDrinks>
+      <Content>
+        {popDrinks.map((drinkPop) => {
+          const { drink, description, drinkThumb, _id } = drinkPop;
+          return (
+            <LinkDrink key={_id} to={`/drink/${_id}`}>
+              <PopDrinkImage
+                src={drinkThumb}
+                alt={description}
+                width={90}
+                height={90}
+              />
+              <div>
+                <NameDrink>{drink}</NameDrink>
+                <DrinkDescription>{description}</DrinkDescription>
+              </div>
+            </LinkDrink>
+          );
+        })}
+      </Content>
+    </AsideContainer>
+  );
+};
+
+export default PopularDrinks;
---
+++ src/components/AddDrink/PopularDrinks/PopularDrinks.styled.jsx
@@ -0,0 +1,107 @@
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+
+export const AsideContainer = styled.aside`
+  @media screen and (min-width: 768px) {
+    max-width: 704px;
+    margin: 0 auto;
+  }
+
+  @media screen and (min-width: 1440px) {
+    max-width: 313px;
+    margin: 0 0 0 auto;
+  }
+`;
+
+export const TitlePopularDrinks = styled.h3`
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 1.33333;
+  color: var(--primary-text-color);
+  margin-bottom: 28px;
+  margin-top: 40px;
+
+  @media screen and (min-width: 768px) {
+    font-size: 24px;
+    margin-bottom: 40px;
+    margin-top: 80px;
+  }
+`;
+
+export const TitleFollowUs = styled.h3`
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 1.33333;
+  color: var(--primary-text-color);
+  margin-bottom: 20px;
+
+  @media screen and (min-width: 768px) {
+    font-size: 24px;
+    margin-bottom: 24px;
+  }
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 24px;
+
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+    flex-wrap: wrap;
+    row-gap: 32px;
+    column-gap: 24px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    flex-direction: column;
+    flex-wrap: nowrap;
+    row-gap: 28px;
+  }
+`;
+
+export const LinkDrink = styled(NavLink)`
+  display: flex;
+  align-items: flex-start;
+  column-gap: 14px;
+  &:hover,
+  &:focus {
+    img {
+      -webkit-box-shadow: 0px 0px 17px 1px rgba(14, 20, 92, 1);
+      -moz-box-shadow: 0px 0px 17px 1px rgba(14, 20, 92, 1);
+      box-shadow: 0px 0px 17px 1px rgba(14, 20, 92, 1);
+    }
+  }
+`;
+
+export const PopDrinkImage = styled.img`
+  border-radius: 8px;
+  transition: all 400ms ease;
+`;
+
+export const NameDrink = styled.h4`
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 1.375;
+  color: var(--primary-text-color);
+  margin-bottom: 8px;
+`;
+
+export const DrinkDescription = styled.p`
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 1.28571;
+  color: var(--secondary-text-color);
+  max-width: 232px;
+  min-height: 56px;
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+
+  @media screen and (min-width: 1440px) {
+    line-height: 1.428;
+  }
+`;
