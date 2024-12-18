37164b8b49d11342b4e3efc50ff9d48df0cd75d6
Halyna Marchenko <marchenkohalyna888@gmail.com>
2024-12-18 18:33:09 +0200

    set up aside popular drinks

--- src/components/AddDrink/PopularDrinks/PopularDrinks.jsx
+++ src/components/AddDrink/PopularDrinks/PopularDrinks.jsx
@@ -1,11 +1,51 @@
-import FollowUs from "../FollowUs/FollowUs"
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchPopularDrinks } from "../../../redux/drinks/drinks.operations";
+import { selectPopularDrinks } from "/src/redux/selectors/drinks.selectors.js";
+import {
+  DrinkDescription,
+  NameDrink,
+  PopDrinkImage,
+  LinkDrink,
+  Content,
+  TitlePopularDrinks,
+  AsideContainer,
+  TitleFollowUs,
+} from "./PopularDrinks.styled";
+import FollowUs from "../FollowUs/FollowUs";
 
-const PopularDrinks = () => {
+export const PopularDrinks = () => {
+  const popDrinks = useSelector(selectPopularDrinks);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchPopularDrinks());
+  }, [dispatch]);
+
   return (
-      <div>
-        <FollowUs />
-    </div>
-  )
-}
-
-export default PopularDrinks
+    <AsideContainer>
+      <TitleFollowUs>Follow Us</TitleFollowUs>
+      <FollowUs />
+      <TitlePopularDrinks>Popular drinks</TitlePopularDrinks>
+      <Content>
+        {popDrinks.map((drinkPop) => {
+          const { drink, description, drinkThumb, _id } = drinkPop;
+          return (
+            <LinkDrink key={_id} to={`/drink/${_id}`}>
+              <PopDrinkImage
+                src={drinkThumb}
+                alt={description}
+                width={90}
+                height={90}
+              />
+              <div>
+                <NameDrink>{drink}</NameDrink>
+                <DrinkDescription>{description}</DrinkDescription>
+              </div>
+            </LinkDrink>
+          );
+        })}
+      </Content>
+    </AsideContainer>
+  );
+};
--- src/pages/AddDrinkPage/AddDrinkPage.jsx
+++ src/pages/AddDrinkPage/AddDrinkPage.jsx
@@ -1,6 +1,6 @@
 import { FormContainer, MainContainer, PopularContainer } from "./AddDrinkPage.styled"
 import AddDrinkForm from "../../components/AddDrinkR/AddDrinkForm"
-import PopularDrinks from "../../components/AddDrink/PopularDrinks/PopularDrinks"
+import { PopularDrinks } from "../../components/AddDrink/PopularDrinks/PopularDrinks"
 const AddDrinkPage = () => {
   return (
       <MainContainer className="container">
