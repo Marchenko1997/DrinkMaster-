5ffaf09c923aa03be77cc09fd5fba168d5553c02
Halyna Marchenko <marchenkohalyna888@gmail.com>
2024-12-23 20:22:02 +0200

    creating footer logo

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import Logo from "../Logo/Logo"
+
+const Footer = () => {
+  return (
+      <div>
+        <Logo />
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Footer/Logo/Logo.jsx
@@ -0,0 +1,10 @@
+import { Icon, IconText, LogoLink } from "./Logo.styled";
+
+export const Logo = () => {
+  return (
+    <LogoLink to="/home" relative="path" onClick={() => window.scrollTo(0, 0)}>
+      <Icon />
+      <IconText>Drink Master</IconText>
+    </LogoLink>
+  );
+};
